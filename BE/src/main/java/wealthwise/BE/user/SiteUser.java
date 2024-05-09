c6cf14810c3dc6e13d06cc7405b6b2c9810b78ca
kanghan03 <ganghan03@pukyong.ac.kr>
2024-05-09 20:38:48 +0900

    BE/#100-로그인_회원가입_기능

---
+++ BE/src/main/java/wealthwise/BE/user/SiteUser.java
@@ -0,0 +1,25 @@
+package com.wealthwise.BE.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
---
+++ BE/src/main/java/wealthwise/BE/user/UserRepository.java
@@ -0,0 +1,6 @@
+package com.wealthwise.BE.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+}
