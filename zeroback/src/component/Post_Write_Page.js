2278e3240d2104b8593e2a9b7b6e5d9443223b67
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-05-13 16:20:32 +0900

    글쓰기 페이지 구현

--- zeroback/src/component/Post_Write_Page.js
+++ zeroback/src/component/Post_Write_Page.js
@@ -1,8 +1,37 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import './Post_Write_Page.css';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+/* import axios from 'axios'; */
 
 function PostWrite() {
+    const navigate = useNavigate();
+
+    const [board, setBoard] = useState({
+        title: '',
+        createdBy: '',
+        contents: '',
+    });
+
+    const { title, createdBy, contents } = board; //비구조화 할당
+
+    const onChange = (event) => {
+        const { value, name } = event.target; //event.target에서 name과 value만 가져오기
+        setBoard({
+            ...board,
+            [name]: value,
+        });
+    };
+
+    const saveBoard = async () => {
+        /* await axios.post(`//localhost:8080/board`, board).then((res) => {
+            alert('등록되었습니다.');
+            navigate('/board');
+        }); */
+    };
+
+    const backToList = () => {
+        navigate('/board');
+    };
+
     return (
         <main>
             <div id="NavBar">
@@ -19,8 +48,37 @@ function PostWrite() {
                 </div>
                 <i id="NavLine"></i>
             </div>
+            <div>
+                <div>
+                    <span>제목</span>
+                    <input type="text" name="title" value={title} onChange={onChange} />
+                </div>
+                <div>
+                    <span>작성자</span>
+                    <input
+                        type="text"
+                        name="createdBy"
+                        value={createdBy}
+                        onChange={onChange}
+                    />
+                </div>
+                <div>
+                    <span>내용</span>
+                    <textarea
+                        name="contents"
+                        cols="30"
+                        rows="10"
+                        value={contents}
+                        onChange={onChange}
+                    ></textarea>
+                </div>
+                <div>
+                    <button onClick={saveBoard}>저장</button>
+                    <button onClick={backToList}>취소</button>
+                </div>
+            </div>
         </main>
     );
-}
+};
 
 export default PostWrite;
