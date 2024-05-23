6de548d67b9cb5f88aeca0e927b659fee1ace9c4
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-05-23 17:07:53 +0900

    지수 띄우기 구현

--- zeroback/src/component/IndexPage/IndexPage.js
+++ zeroback/src/component/IndexPage/IndexPage.js
@@ -1,48 +1,16 @@
-// TradingViewWidget.jsx
-import { Route } from "react-router-dom"
-import React, { useEffect, useRef, memo } from 'react';
-
-function TradingViewWidget() {
-  const container = useRef();
-
-  useEffect(
-    () => {
-      const script = document.createElement("script");
-      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = `
-        {
-          "width": "800",
-          "height": "550",
-          "symbol": "NASDAQ:AAPL",
-          "interval": "W",
-          "timezone": "Asia/Seoul",
-          "theme": "light",
-          "style": "0",
-          "locale": "kr",
-          "enable_publishing": false,
-          "hide_top_toolbar": true,
-          "allow_symbol_change": false,
-          "calendar": false,
-          "hide_volume": true,
-          "support_host": "https://www.tradingview.com"
-        }`;
-      container.current.appendChild(script);
-    }
-  );
-
-  return (
-    <div className="tradingview-widget-container" ref={container} />
-  );
-}
+import NavBar from "../PublicComponent/NavBar";
+import TradingViewWidget from "./test";
+import "./IndexPage.css"
 
 function IndexPage() {
   return (
     <main>
-      <Route path="/index" element={<TradingViewWidget />}></Route>
+      <NavBar />
+      <div className="index-box">
+        <TradingViewWidget />
+      </div>
     </main>
   )
 }
 
-export default memo(TradingViewWidget);
+export default IndexPage;
