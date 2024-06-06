92ce8e19e007837cf960e8abe425edc3f3db3e2a
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-06-06 18:28:53 +0900

    프로필 코드 수정

--- FE/src/pages/ProfilePage.js
+++ FE/src/pages/ProfilePage.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import '../styles/form.css';
 
 function ProfilePage() {
 
     const navigate = useNavigate();
+    const [nickname, setNickname] = useState([]);
+    const [id, setId] = useState([]);
 
     const signOut = () => {
         localStorage.removeItem("token_key");
         navigate("/");
     };
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/users/profile")
+            .then(response => {
+                setNickname(response.nickname);
+                setId(response.id)
+            });
+    }, []);
 
     return (
         <div id="form">
@@ -20,10 +32,10 @@ function ProfilePage() {
             <h1 id="form-title">Profile</h1>
             <div id="login-input_box">
                 <text className="form-text" required>
-                    닉네임:
+                    닉네임: {nickname}
                 </text>
                 <text className="form-text" required>
-                    ID:
+                    ID: {id}
                 </text>
             </div>
             <button id="form-button" type="submit" onClick={signOut}>로그아웃</button>
