392a95dd56e81251e0453c47351e8daa49f75025
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-06-07 01:07:58 +0900

    라우팅 수정

--- FE/src/App.js
+++ FE/src/App.js
@@ -19,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/board" element={<BoardPage />} />
+        <Route path="/index/:chartId" element={<IndexPage />} />
         <Route path="/index" element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/post" element={<PostPage />} />
